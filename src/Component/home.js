f85e47b4d3c135da74c31483da298d2b7c9994bd
akram <akramraza962@gmail.com>
2020-05-12 02:24:32 +0530

    React router dom app

---
+++ src/Component/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+                <p>Lorem ipsum dolor sit amet, vis et justo hendrerit delicatissimi. Option minimum accusata ne vix, et sed epicurei adversarium. Modus dignissim cu vel, eum elit probo omnesque et. Vim ei delenit oportere, mei sint suscipit ea.
+
+                Te pri sint deseruisse dissentias, propriae recteque no ius. At quo purto accusamus. Aliquid scribentur cum ut. Mundi consul verterem no sed, no cum summo deserunt. Quem corpora duo ei.
+
+                Quo omnes tibique ad, euismod fabellas ea cum. Deleniti efficiantur et usu, summo disputationi duo ut. Consulatu patrioque id cum. Et ius similique constituam, legere oblique sapientem vel ea. Prima habemus ea pri. Est no natum animal scripserit, aliquid sanctus cu ius, nostro aliquam an pri. Ei commodo liberavisse nam.
+
+                Et splendide tincidunt nam, no vim alia audire. Nam rebum dolorem ex, cibo salutandi id per. Vix esse quando fierent et. Utinam posidonium ei nam. Pri no quando ornatus recteque, postea apeirian periculis et sed.
+
+                Vel at quem possit instructior. Eu ipsum atomorum mea, pri et amet malorum verterem. Laudem perfecto mandamus eu sit, vel ut ubique putant platonem. Nam suas tota vocibus ex, at vel veritus blandit quaerendum. Ei choro nostrum vel, mea id denique erroribus.</p>
+        </div>
+
+    )
+}
+export default Home;
