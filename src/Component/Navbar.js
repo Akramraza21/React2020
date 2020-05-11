763b28469fd1e45e4104927effe3db847605a01e
akram <akramraza962@gmail.com>
2020-05-12 02:47:26 +0530

    Link and NavLink tag added

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
 
+
+//NavLink create an active class where as Link does not create That
+//NavLink and Link both have same task to stop rerender the dom. 
+// it is used insted of anchor and href tag 
+
+  
 const Navbar = () => {
+    const style = {
+            "background-color":"yellow"
+    } 
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a className="brand-logo">Akatsuki Poketime's</a>
                 <ul className="right">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
