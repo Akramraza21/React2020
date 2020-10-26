76dd781b6890f9d5c3c646c12b82d99102518ef2
akram <akramraza962@gmail.com>
2020-10-26 20:07:55 +0530

    fetch from api

--- src/component/graph.js
+++ src/component/graph.js
@@ -3,32 +3,39 @@ import axios from 'axios'
 export default class graph extends Component {
     constructor(){
         super();
-        this.state = {data:null,user_id:1,show:false}
+        this.state = {postCommentData:null,post:null,comment:null,user_id:1,show:false}
     }
     // componentDidMount(){
         
     //     if(this.state.user_id){
     //         axios.get(`http://52.175.201.248:3000/facebook/facebook_post_comments/${this.state.user_id}`)
-    //         .then(resp=>this.setState({data:resp.data}))
+    //         .then(resp=>this.setState({postCommentData:resp.postCommentData}))
     //         .catch(error=>console.log(error))
     //     }
     // }
-  clickHandler (){
+  clickHandler () {
       axios.get(`http://52.175.201.248:3000/facebook/facebook_post_comments/${this.state.user_id}`)
-            .then(resp=>this.setState({data:resp.data}))
+            .then(resp=>this.setState({postCommentData:resp.data}))
+            .catch(error=>console.log(error))
+
+    axios.get(`http://52.175.201.248:3000/facebook/facebook_post/${this.state.user_id}`)
+            .then(resp=>this.setState({post:resp.data}))
+            .catch(error=>console.log(error))
+    axios.get(`http://52.175.201.248:3000/facebook/facebook_comments/${this.state.user_id}`)
+            .then(resp=>this.setState({comment:resp.data}))
             .catch(error=>console.log(error))
     //   let show = this.state.show
     //   this.setState({show:!show})
-    //   console.log(this.state.data);
+    //   console.log(this.state.postCommentData);
   }
     render() {
-        let {data} = this.state
-        // console.log(data);
+        let {postCommentData} = this.state
+        console.log(this.state);
         return (
             <div>
                 <input placeholder = "User id" onChange={event=>this.setState({user_id:event.target.value})}/>
                 <button onClick={()=>this.clickHandler()}>Fetch Data</button>
-                {data?data.list.map((res)=><div key={res._id}> <p> post id : {res.post_id}  &npsc post comment id: {res.comment_id}</p></div>):null}
+                {postCommentData?postCommentData.list.map((res)=><div key={res._id}> <p> post id : {res.post_id}  post comment id: {res.comment_id}</p></div>):null}
             </div>
         )
     }
